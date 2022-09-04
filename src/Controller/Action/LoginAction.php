b5bb179b4a01cda89a1c0347428e6fe3fa6974b3
TymekB <baran.tymoteusz@gmail.com>
2022-09-04 12:16:31 +0200

    Add form_login authentication

--- src/Controller/Action/LoginAction.php
+++ src/Controller/Action/LoginAction.php
@@ -5,18 +5,27 @@ declare(strict_types=1);
 namespace App\Controller\Action;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Twig\Environment;
 
 final class LoginAction
 {
-    public function __construct(private Environment $twig)
+    public function __construct(private Environment $twig, private AuthenticationUtils $authenticationUtils)
     {
     }
 
     public function __invoke(): Response
     {
+        $lastUsername = $this->authenticationUtils->getLastUsername();
+        $error = $this->authenticationUtils->getLastAuthenticationError();
+
         return new Response(
-            $this->twig->render('login.html.twig')
+            $this->twig->render('login.html.twig',
+                [
+                    'last_username' => $lastUsername,
+                    'error' => $error
+                ]
+            ),
         );
     }
 }
