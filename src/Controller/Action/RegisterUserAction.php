5349ffe89dc1e598eb23c7326bbf617cf0de355e
TymekB <baran.tymoteusz@gmail.com>
2022-09-05 23:10:51 +0200

    Add dispatching CreateUserCommand when registration form is submitted

--- src/Controller/Action/RegisterUserAction.php
+++ src/Controller/Action/RegisterUserAction.php
@@ -4,22 +4,35 @@ declare(strict_types=1);
 
 namespace App\Controller\Action;
 
+use App\Command\CreateUserCommand;
 use App\Form\Type\RegistrationFormType;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Twig\Environment;
 
 final class RegisterUserAction
 {
     public function __construct(
         private readonly Environment $twig,
-        private readonly FormFactoryInterface $formFactory
+        private readonly FormFactoryInterface $formFactory,
+        private readonly MessageBusInterface $messageBus
     ) {}
 
-
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         $form = $this->formFactory->create(RegistrationFormType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $createUserCommand = new CreateUserCommand(
+                $form->get('email')->getData(),
+                $form->get('password')->getData()
+            );
+
+            $this->messageBus->dispatch($createUserCommand);
+        }
 
         return new Response(
             $this->twig->render('register.html.twig',
@@ -29,5 +42,4 @@ final class RegisterUserAction
             )
         );
     }
-
 }
