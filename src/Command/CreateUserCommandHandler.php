e80793e9c8d48547ea32e79168c33ffba613ae8b
tymekb <baran.tymoteusz@gmail.com>
2022-09-08 14:13:23 +0200

    Add creating user

--- src/Command/CreateUserCommandHandler.php
+++ src/Command/CreateUserCommandHandler.php
@@ -4,12 +4,32 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class CreateUserCommandHandler implements MessageHandlerInterface
 {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {}
+
     public function __invoke(CreateUserCommand $createUserCommand)
     {
+        $user = new User();
 
+        $email = $createUserCommand->getEmail();
+        $password = $this->passwordHasher->hashPassword(
+            $user,
+            $createUserCommand->getPassword(),
+        );
+
+        $user->setEmail($email);
+        $user->setPassword($password);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
     }
 }
