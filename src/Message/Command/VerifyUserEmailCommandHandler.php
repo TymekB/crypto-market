d9ea2e3373f4ec9e6ce182480f26fae6b1eeb585
TymekB <baran.tymoteusz@gmail.com>
2022-09-13 12:45:56 +0200

    Add VerifyUserEmailCommandHandler

---
+++ src/Message/Command/VerifyUserEmailCommandHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message\Command;
+
+final class VerifyUserEmailCommandHandler
+{
+    public function __invoke(VerifyUserEmailCommand $verifyUserEmailCommand)
+    {
+
+    }
+}
