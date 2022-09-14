d9eabc34db81f8a98a51649af61a005a7e8c5a81
TymekB <baran.tymoteusz@gmail.com>
2022-09-14 14:09:52 +0200

    Add EnableUserOnUserVerifiedEventHandler

---
+++ src/Message/Event/EnableUserOnUserVerifiedEventHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message\Event;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+final class EnableUserOnUserVerifiedEventHandler
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager
+    ) {}
+
+    public function __invoke(UserVerifiedEvent $userVerifiedEvent): void
+    {
+        /** @var UserRepository $userRepository */
+        $userRepository = $this->entityManager->getRepository(User::class);
+        $userId = $userVerifiedEvent->getId();
+
+        $user = $userRepository->find($userId);
+        $user->setEnabled(true);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+}
