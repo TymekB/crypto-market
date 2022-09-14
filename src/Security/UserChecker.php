6ff6d9181b05dce0e8bf2c4f38293f4feee3a126
TymekB <baran.tymoteusz@gmail.com>
2022-09-14 14:55:25 +0200

    Disable logging in if user did not confirm email

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use App\Entity\User;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+final class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->isEnabled()) {
+            throw new CustomUserMessageAccountStatusException(
+                'You have to confirm your email.'
+            );
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user)
+    {
+    }
+}
