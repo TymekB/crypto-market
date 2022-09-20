cef072d4888e01f337f62b758e678d8fa384ac77
TymekB <baran.tymoteusz@gmail.com>
2022-09-20 13:01:14 +0200

    Add testIfUserIsCreated integration test for CreateUserCommand

---
+++ tests/Message/Command/CreateUserCommandTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Message\Command;
+
+use App\Entity\User;
+use App\Message\Command\CreateUserCommand;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class CreateUserCommandTest extends KernelTestCase
+{
+    private readonly MessageBusInterface $messageBus;
+    private readonly EntityManagerInterface $entityManager;
+
+    public function setUp(): void
+    {
+        self::bootKernel();
+        $container = self::getContainer();
+
+        $this->messageBus = $container->get('command.bus');
+        $this->entityManager = $container->get('doctrine.orm.entity_manager');
+    }
+
+    public function testIfUserIsCreated(): void
+    {
+        $this->messageBus->dispatch(
+            new CreateUserCommand('test@example.com', 'test')
+        );
+
+        $user = $this->entityManager
+            ->getRepository(User::class)
+            ->findOneBy(
+                [
+                    'email' => 'test@example.com'
+                ]
+            );
+
+        $this->assertNotNull($user);
+        $this->assertEquals('test@example.com', $user->getEmail());
+    }
+}
