aff950b5252d01a94011807ac32e254ba5f8ee0e
TymekB <baran.tymoteusz@gmail.com>
2022-09-25 13:25:33 +0200

    Add ResetUserPasswordCommandHandler

---
+++ src/Message/Command/ResetUserPasswordCommandHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message\Command;
+
+final class ResetUserPasswordCommandHandler
+{
+    public function __invoke(ResetUserPasswordCommand $resetUserPasswordCommand)
+    {
+
+    }
+}
