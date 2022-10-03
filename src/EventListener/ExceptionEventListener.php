f97c8a306edf1979e0ad406cc72e3afdf823180a
TymekB <baran.tymoteusz@gmail.com>
2022-10-03 17:17:55 +0200

    Add AsEventListener attribute to ExceptionEventListener

--- src/EventListener/ExceptionEventListener.php
+++ src/EventListener/ExceptionEventListener.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\Routing\RouterInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
+#[AsEventListener(event: ExceptionEvent::class)]
 final class ExceptionEventListener
 {
     public function __construct(
         private readonly RouterInterface $router,
     ) {}
 
-    public function onKernelException(ExceptionEvent $event)
+    public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
         $flashbag = $event->getRequest()->getSession()->getFlashbag();
