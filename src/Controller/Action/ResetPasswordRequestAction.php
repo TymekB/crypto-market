fcec0b1a8804ef5b77cd9f51eced1decdff8a92e
TymekB <baran.tymoteusz@gmail.com>
2022-10-03 18:08:20 +0200

    Add dispatching ResetUserPasswordCommand

--- src/Controller/Action/ResetPasswordRequestAction.php
+++ src/Controller/Action/ResetPasswordRequestAction.php
@@ -5,25 +5,35 @@ declare(strict_types=1);
 namespace App\Controller\Action;
 
 use App\Form\Type\ResetPasswordRequestFormType;
+use App\Message\Command\ResetUserPasswordCommand;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Twig\Environment;
 
 final class ResetPasswordRequestAction
 {
     public function __construct(
         private readonly Environment          $twig,
-        private readonly FormFactoryInterface $formFactory
+        private readonly FormFactoryInterface $formFactory,
+        private readonly MessageBusInterface  $commandBus
     )
     {
     }
-
 
     public function __invoke(Request $request): Response
     {
         $form = $this->formFactory->create(ResetPasswordRequestFormType::class);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userEmail = $form->get('email')->getData();
+
+            $this->commandBus->dispatch(
+                new ResetUserPasswordCommand($userEmail)
+            );
+        }
 
         return new Response(
             $this->twig->render('reset_password/request.html.twig',
@@ -33,5 +43,4 @@ final class ResetPasswordRequestAction
             )
         );
     }
-
 }
