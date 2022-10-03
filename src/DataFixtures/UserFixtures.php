02d0d1e46a05bc48e63de0a0acaa426def84d4b2
TymekB <baran.tymoteusz@gmail.com>
2022-10-03 17:41:10 +0200

    Move UserFactory to Fixtures directory

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\DataFixtures;
 
-use App\Factory\UserFactory;
+use App\Factory\Fixtures\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
--- src/Factory/Fixtures/UserFactory.php
+++ src/Factory/Fixtures/UserFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Factory;
+namespace App\Factory\Fixtures;
 
 use App\Entity\User;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
