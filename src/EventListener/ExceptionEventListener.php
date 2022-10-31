ee6a282e18348daec066cd91303c9fe78fb074b0
TymekB <baran.tymoteusz@gmail.com>
2022-10-31 16:59:44 +0100

    Add checking if HandlerFailedException was thrown in ExceptionEventListener

--- src/EventListener/ExceptionEventListener.php
+++ src/EventListener/ExceptionEventListener.php
@@ -7,6 +7,7 @@ namespace App\EventListener;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Routing\RouterInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
@@ -15,15 +16,24 @@ final class ExceptionEventListener
 {
     public function __construct(
         private readonly RouterInterface $router,
-    ) {}
+    )
+    {
+    }
 
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
         $flashbag = $event->getRequest()->getSession()->getFlashbag();
 
-        if ($exception->getPrevious() instanceof VerifyEmailExceptionInterface) {
-            $flashbag->add('danger', 'Something went wrong with verifying your email. Try again later.');
+        if ($exception instanceof HandlerFailedException) {
+            $exception = $exception->getPrevious();
+        }
+
+        if ($exception instanceof VerifyEmailExceptionInterface) {
+            $flashbag->add(
+                'danger',
+                'Something went wrong with verifying your email. Try again later.'
+            );
 
             $response = new RedirectResponse(
                 $this->router->generate('login')
