8eb3450c8d3c89a4bdd265aa8ac7f31555874794
TymekB <baran.tymoteusz@gmail.com>
2022-11-01 11:34:44 +0100

    Add UserFactory to CreateUserCommandHandler

--- src/Message/CommandHandler/CreateUserCommandHandler.php
+++ src/Message/CommandHandler/CreateUserCommandHandler.php
@@ -4,35 +4,28 @@ declare(strict_types=1);
 
 namespace App\Message\CommandHandler;
 
-use App\Entity\User;
+use App\Factory\UserFactoryInterface;
 use App\Message\Command\CreateUserCommand;
 use App\Message\Event\UserCreatedEvent;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsMessageHandler]
 final class CreateUserCommandHandler
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly UserFactoryInterface $userFactory,
         private readonly MessageBusInterface $eventBus
     ) {}
 
-    public function __invoke(CreateUserCommand $createUserCommand)
+    public function __invoke(CreateUserCommand $createUserCommand): void
     {
-        $user = new User();
-
-        $email = $createUserCommand->getEmail();
-        $password = $this->passwordHasher->hashPassword(
-            $user,
-            $createUserCommand->getPassword(),
+        $user = $this->userFactory->createUser(
+            $createUserCommand->getEmail(),
+            $createUserCommand->getPassword()
         );
-
-        $user->setEmail($email);
-        $user->setPassword($password);
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
