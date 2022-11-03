4fbccd8c686cdb15a6bf4d023be6b9ff75232788
TymekB <baran.tymoteusz@gmail.com>
2022-11-03 21:41:47 +0100

    Add showing cryptocurrency list in the dashboard

--- src/Controller/Action/ShowDashboardAction.php
+++ src/Controller/Action/ShowDashboardAction.php
@@ -4,19 +4,31 @@ declare(strict_types=1);
 
 namespace App\Controller\Action;
 
+use App\API\BinanceInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
 final class ShowDashboardAction
 {
     public function __construct(
-        private readonly Environment $twig
+        private readonly Environment $twig,
+        private readonly BinanceInterface $binanceAPI
     ) {}
 
 
     public function __invoke(): Response
     {
-        return new Response($this->twig->render('dashboard.html.twig'));
+        $cryptoCurrencyList = $this->binanceAPI->getPrices([
+            'BTCUSDT',
+            'ETHUSDT',
+            'LTCUSDT'
+        ]);
+
+        return new Response($this->twig->render('dashboard.html.twig',
+            [
+                'cryptoCurrencyList' => $cryptoCurrencyList
+            ]
+        ));
     }
 
 }
