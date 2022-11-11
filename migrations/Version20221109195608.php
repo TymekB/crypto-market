f617e3306e1289dc138403c4f4c3392891d5ee8a
TymekB <baran.tymoteusz@gmail.com>
2022-11-11 13:03:50 +0100

    Add migration

---
+++ migrations/Version20221109195608.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221109195608 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE crypto_currency (id VARCHAR(255) NOT NULL, user_id VARCHAR(255) DEFAULT NULL, symbol VARCHAR(255) NOT NULL, quantity DOUBLE PRECISION NOT NULL, INDEX IDX_59320B70A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE crypto_currency ADD CONSTRAINT FK_59320B70A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE crypto_currency DROP FOREIGN KEY FK_59320B70A76ED395');
+        $this->addSql('DROP TABLE crypto_currency');
+    }
+}
