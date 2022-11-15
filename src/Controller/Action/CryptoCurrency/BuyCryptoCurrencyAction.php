3e5df7b59a4a24d619d89e6ea8a75f1dd1e57708
TymekB <baran.tymoteusz@gmail.com>
2022-11-15 23:03:42 +0100

    Add redirecting user to the dashboard after buying cryptocurrency

--- src/Controller/Action/CryptoCurrency/BuyCryptoCurrencyAction.php
+++ src/Controller/Action/CryptoCurrency/BuyCryptoCurrencyAction.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller\Action\CryptoCurrency;
 
-use App\API\Binance\CryptoCurrencyManagerInterface;
+use App\API\Binance\CryptoCurrency\Manager\CryptoCurrencyManagerInterface;
 use App\Entity\User;
 use App\Form\Type\CryptoCurrencyFormType;
 use App\Message\Command\BuyCryptoCurrencyCommand;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Twig\Environment;
 
@@ -21,7 +23,8 @@ final class BuyCryptoCurrencyAction
         private readonly Environment $twig,
         private readonly CryptoCurrencyManagerInterface $cryptoCurrencyManager,
         private readonly FormFactoryInterface $formFactory,
-        private readonly MessageBusInterface $messageBus
+        private readonly MessageBusInterface $messageBus,
+        private readonly RouterInterface $router
     )
     {
     }
@@ -39,6 +42,8 @@ final class BuyCryptoCurrencyAction
             $this->messageBus->dispatch(
                 new BuyCryptoCurrencyCommand($symbol, $quantity, $userId)
             );
+
+            return new RedirectResponse($this->router->generate('dashboard'));
         }
 
         $cryptoCurrencyPrice = $this->cryptoCurrencyManager
