8e0ca604be4a266972bc91d8694dafa3acc2f825
TymekB <baran.tymoteusz@gmail.com>
2022-12-07 13:17:38 +0100

    Add SellCryptoCurrencyCommand

---
+++ src/Message/Command/SellCryptoCurrencyCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message\Command;
+
+final class SellCryptoCurrencyCommand
+{
+    public function __construct(
+        private readonly string $symbol,
+        private readonly float $quantity,
+        private readonly string $userId,
+    )
+    {
+    }
+
+    public function getSymbol(): string
+    {
+        return $this->symbol;
+    }
+
+    public function getUserId(): string
+    {
+        return $this->userId;
+    }
+
+    public function getQuantity(): float
+    {
+        return $this->quantity;
+    }
+}
