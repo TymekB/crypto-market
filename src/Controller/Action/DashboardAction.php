2d047d3d3d1fe9c5ec82537f985667555982e44f
TymekB <baran.tymoteusz@gmail.com>
2022-12-07 14:04:11 +0100

    Rename ShowDashboardAction to DashboardAction

--- src/Controller/Action/DashboardAction.php
+++ src/Controller/Action/DashboardAction.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Twig\Environment;
 
-final class ShowDashboardAction
+final class DashboardAction
 {
     public function __construct(
         private readonly Environment $twig,
@@ -22,7 +22,6 @@ final class ShowDashboardAction
         /** @var User $user */
         $wallet = $this->walletFactory->create($user);
 
-        /** @var User $user */
         return new Response($this->twig->render('dashboard.html.twig',
             [
                 'wallet' => $wallet,
