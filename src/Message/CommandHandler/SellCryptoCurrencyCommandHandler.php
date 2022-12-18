54bbd372a3368b0c660a218aed06feabda95e917
TymekB <baran.tymoteusz@gmail.com>
2022-12-18 13:13:40 +0100

    Add dispatching TransactionCompletedEvent in SellCryptoCurrencyCommandHandler

--- src/Message/CommandHandler/SellCryptoCurrencyCommandHandler.php
+++ src/Message/CommandHandler/SellCryptoCurrencyCommandHandler.php
@@ -6,16 +6,19 @@ namespace App\Message\CommandHandler;
 
 use App\Entity\User;
 use App\Message\Command\SellCryptoCurrencyCommand;
+use App\Message\Event\TransactionCompletedEvent;
 use App\User\CryptoCurrency\Manager\UserCryptoCurrencyManagerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler]
 final class SellCryptoCurrencyCommandHandler
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
-        private readonly UserCryptoCurrencyManagerInterface $userCryptoCurrencyManager
+        private readonly UserCryptoCurrencyManagerInterface $userCryptoCurrencyManager,
+        private readonly MessageBusInterface $eventBus
     )
     {
     }
@@ -30,6 +33,10 @@ final class SellCryptoCurrencyCommandHandler
             ->getRepository(User::class)
             ->find($userId);
 
-        $this->userCryptoCurrencyManager->sell($user, $symbol, $quantity);
+        $transactionSummaryDto = $this->userCryptoCurrencyManager->sell($user, $symbol, $quantity);
+
+        $this->eventBus->dispatch(
+            TransactionCompletedEvent::fromTransactionSummaryDto($transactionSummaryDto)
+        );
     }
 }
